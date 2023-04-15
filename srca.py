e9b3e27795e07d133f6ecddf528c92e863bb8d31
RudyLopezJr <rudylopezjr_01@hotmail.com>
2023-04-15 13:06:02 -0500

    resultados 2

--- srca.py
+++ srca.py
@@ -37,8 +37,6 @@ class App(ctk.CTk):
         self.estudiantePlan = ""
         
         self.cambiarFrame(Inicio, "Inicio")
-        self.cambiarFrame(Resultados, "Resultados")
-      
 
     def cambiarRuta(self,nuevaRuta):
         self.frames[nuevaRuta].tkraise()
